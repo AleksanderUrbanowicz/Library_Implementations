e0b4af535f5ae0cc9a38d71c91ba21740e0727be
AleksanderUrbanowicz <57544016+AleksanderUrbanowicz@users.noreply.github.com>
2020-01-07 18:50:55 +0100

    executor test

---
+++ Assets/ExecutorImplementation.cs
@@ -0,0 +1,13 @@
+﻿using Managers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripts : IExecutor
+{
+    public void Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
