d14b708da1ff62a68b7e424dddcac4d9c2d9b547
AleksanderUrbanowicz <57544016+AleksanderUrbanowicz@users.noreply.github.com>
2020-01-07 19:21:24 +0100

    new executor
    new executor

---
+++ Assets/ExecutorImplementation1.cs
@@ -0,0 +1,13 @@
+﻿using Managers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripts2 : IExecutor
+{
+    public void Execute()
+    {
+        
+    }
+
+}
